1e4137f521c6f07f7d7892fefb4ddc36f5fd2f0a
VovaKrut123 <125643076+VovaKrut123@users.noreply.github.com>
2023-10-02 13:40:01 +0700

    Update main.js

--- main.js
+++ main.js
@@ -251,6 +251,85 @@
 
 // alert(arr2.join('')); 
 
+//4 лаба 1 часть 
+// Задача 1.1
+// const arr = [];
+// arr.length = prompt("Введите длину массива");
+// for (let i = 0; i <= arr.length-1; i++){
+//     arr[i] = Math.floor(Math.random() * 100);
+// }
+// alert(arr);
+// arr.sort((a,b) => a-b);
+// alert(arr[arr.length-1] - arr[0]);
+
+// Задача 1.2
+// const arr = [1, 4, 34, 1, 20, 6, 6, 12, 8, 6];
+// let arr1 = new Set(arr);
+// arr1 = [...arr1]
+// alert(arr1);
+
+// Задача 1.3
+// const arr = 
+//     [
+//     {id: 1, idDone: true}, 
+//     {id: 2, idDone: false},
+//     {id: 3, idDone: true}
+//     ]
+// for(let i = 0; i<=arr.length-1;i++){
+//     if(arr[i].idDone === true){
+//         console.log(arr[i]);
+//     }
+// }
+
+// Задача 2.1
+// const arr = [];
+// let k = prompt("Введите чило для поиска");
+// arr.length = prompt("Введите длину массива");
+// for (let i = 0; i <= arr.length-1; i++){
+//     arr[i] = Math.floor(Math.random() * 10);
+// }
+// alert(arr);
+// alert(arr.filter(item => item > k));
+
+// Задача 2.2
+// const arr = [1, 4, [34, 1, 20], [6, [6, 12, 8], 6]];
+// var arr2 = [].concat(arr);
+// alert(arr2);
+
+// Задача 3.1
+// const arr = [];
+// console.log(arr);
+// let count = 0;
+// for (let i = 0; i <= arr.length-1; i++){
+//     for (let j = i+1; j <= arr.length-1;j++){
+//         if (arr[i] + arr[j] === 0){
+//             count++;
+//             arr.splice(j, 1);
+//             arr.splice(i, 1);
+//             i--;
+//         }
+//     }
+// }
+// alert(count + " пары чисел, дающих 0");
+
+// Задача 3.2
+// const arr = [];
+// console.log(arr);
+// let count = 0;
+// for (let i = 0; i <= arr.length-1; i++){
+//         for (let j = i+1; j <= arr.length-1;j++){
+//             for (let k = j+1; k <= arr.length-1; k++){
+//                 if (arr[i] + arr[j] + arr[k] === 0){
+//                     count++;
+//                     arr.splice(k, 1);
+//                     arr.splice(j, 1);
+//                     arr.splice(i, 1);
+//                 }
+//             }
+//         }
+// }
+// alert(count + " пары чисел, дающих 0");
+
 //4 ЛАБА 2 ЧАСТЬ
 // Задача 1.1
 // function* randomizer(){
